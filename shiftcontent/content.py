67cd186f06281dedd599d231067c004b683a5150
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2018-04-08 10:56:14 +0100

    Some initial testing

--- shiftcontent/content.py
+++ shiftcontent/content.py
@@ -47,47 +47,97 @@ class ContentItem:
 
 import os
 import yaml
+import hashlib
 from pprint import pprint as pp
 from shiftcontent import exceptions as x
+from shiftcontent import schema as schemas
 
 
 class ContentService:
 
-    schema = None
-
-    def __init__(self, schema_path):
+    def __init__(self, schema_path: str, known_path: str) -> None:
         """
         Initialise service
-        :param schema_path: str, yaml schema file path
+        :param schema_path: str, yaml definition file path
+        :param known_path: str, where to store known schemas
         """
-        self.schema = self.load_schema(schema_path)
+        self.schema_path: str = schema_path
+        self._known_schemas_path: str = known_path
+        self._schema: dict = None
 
-    def load_schema(self, schema_path):
+    @property
+    def known_schemas(self) -> str:
         """
-        Load schema
-        Loads a schema from a yaml file
+        Known schemas
+        Returns path to known schemas. Will check directory existence and
+        create one if necessary .
+        :return: str
+        """
+        path = self._known_schemas_path
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+        return path
+
+    @property
+    def schema(self) -> dict:
+        """
+        Schema
+        Returns current schema. If none is found will load from a definition.
+        :return: dict
+        """
+        if not self._schema:
+            self._schema = self.load_definition(self.schema_path)
+        return self._schema
+
+    def load_definition(self, schema_path) -> dict:
+        """
+        Load definition
+        Loads a definition from a yaml file
         :param schema_path: str, yaml file path
-        :return:
+        :return: dict
         """
         if not os.path.exists(schema_path):
-            msg = 'Unable to locate schema file at path [{}]'
+            msg = 'Unable to locate definition file at path [{}]'
             raise x.ConfigurationException(msg.format(schema_path))
+
+        # read current schema
         with open(schema_path) as yml:
-            yml = yaml.load(yml)
-            schema = {t['handle'].lower(): t for t in yml['content']}
+            text = yml.read()
+
+
+        hash = hashlib.md5(str(text).encode('utf-8')).hexdigest()
+        # known_schema = os.path.join(self.known_schemas, hash + '.yml')
+
+        # print(known_schema)
+
+
+
+        yml = yaml.load(text)
+        schema = {t['handle'].lower(): t for t in yml['content']}
+
+
 
         return schema
 
-    def process_schema(self, schema):
+    def process_definition(self, definition: dict) -> None:
         """
-        Process schema
-        Performs schema syntax validation adn returns a nested dictionary of
-        errors if schema is invalid.
+        Process definition
+        Performs definition syntax validation adn returns a nested dictionary of
+        errors if definitiuon is invalid.
 
-        :param schema: dict, schema
+        :param definition: dict, definition
         :return:
         """
-        pass
+        errors = []
+
+        for content_type in definition.items():
+            schema = schemas.DefinitionSchema()
+            valid = schema.process(content_type)
+
+            for field in content_type['fields']:
+                field_schema = schemas.FieldSchema()
+                field_valid = field_schema.valid
+
 
 
 
--- shiftcontent/tests/content_service_test.py
+++ shiftcontent/tests/content_service_test.py
@@ -2,6 +2,7 @@ import unittest
 from nose.plugins.attrib import attr
 
 import os
+import shutil
 from pprint import pprint as pp
 from shiftcontent.content import ContentService
 from shiftcontent import exceptions as x
@@ -10,32 +11,50 @@ from shiftcontent import exceptions as x
 @attr('content')
 class ContentServiceTest(unittest.TestCase):
 
-    def shchema_path(self):
+    @property
+    def schema_path(self):
         """ Get path to content schema file """
         path = os.path.join(os.getcwd(), 'shiftcontent', 'content.yml')
         return path
+
+    @property
+    def known_path(self):
+        """ Get path to known schemas """
+        return os.path.join(
+            os.getcwd(), 'var', 'data', 'tests', 'known_schemas'
+        )
+
+    def tearDown(self):
+        """ Cleanup """
+        super().tearDown()
+        if os.path.exists(self.known_path):
+            shutil.rmtree(self.known_path)
 
     # --------------------------------------------------------------------------
 
     def test_create_content_service(self):
         """ Creating content service"""
-        service = ContentService(self.shchema_path())
+        service = ContentService(self.schema_path, self.known_path)
         self.assertIsInstance(service, ContentService)
+
+    def test_create_directory_for_known_schemas(self):
+        """ Use know schemas property to create a directory"""
+        path = self.known_path
+        service = ContentService(
+            schema_path=self.schema_path,
+            known_path=path
+        )
+
+        self.assertFalse(os.path.exists(path))
+        self.assertTrue(os.path.exists(service.known_schemas))
 
     def test_raise_when_unable_to_find_schema(self):
         """ Content service raises exception when unable to find schema file"""
+        service = ContentService(
+            schema_path=self.schema_path,
+            known_path=self.known_path
+        )
         with self.assertRaises(x.ConfigurationException):
-            ContentService('bullshit')
+            service.load_definition('/nothing/here')
 
-    @attr('zzz')
-    def test_loading_yaml_schema_on_instantiation(self):
-        """ Content service loads yaml schema on instantiation"""
-        service = ContentService(self.shchema_path())
-        self.assertIsNotNone(service.schema)
-        pp(service.schema)
-
-
-    def can_create_item_of_type(self):
-        """ Can create content item """
-        pass
 
