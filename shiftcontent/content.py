11b05bba72c253df90a8a96d187197b8b9010c6e
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2018-04-27 15:20:30 +0100

    Designing good inteface is hard, man!

--- shiftcontent/content.py
+++ shiftcontent/content.py
@@ -1,57 +1,14 @@
+# todo: meta must be an object to be inheritable (do we need inheritance)
+# todo: is content item an object as well?
+# todo: if we are dealing with objects are we going full-orm?
+# todo: is it a good idea to put custom meta fields into schema?
+# todo: if we do that, can we go without ORM? <- that'd be perfect
 
+# todo: on the other hand orm is great because w e can do migrations!
+# todo: orm is also makes graphql easier (but does it?)
 
-
-
-
-
-
-
-
-class ContentType():
+class EventStore:
     pass
-
-class ContentMeta():
-
-    # todo: meta must be an object to be inheritable
-    # todo: is content item an object as well?
-    # todo: if we are dealing with objects are we going full-orm?
-    # todo: is it a good idea to put custom meta fields into schema?
-    # todo: if we do that, can we go without ORM? <- that'd be perfect
-
-    id = None
-    parent = None
-    children = []
-    fields = {
-        'title': None,
-        'description': None,
-        'keywords': None
-    }
-
-    def __init__(self):
-        self.fields['title'] = ''
-
-    def to_dict(self):
-        return dict(
-            id=self.id,
-
-        )
-
-
-class ContentItem:
-    id = None
-    type = None
-    version = None
-    meta = ContentMeta()
-    fields = {
-        'handle': 'value'
-    }
-
-    # todo: content item must be serializable and de-serializable
-
-
-
-
-
 
 
 class ContentService:
