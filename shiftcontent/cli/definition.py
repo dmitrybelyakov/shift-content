30443ab85cf76ee0a98cab1e0b5666ae62c3efaa
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2018-08-10 14:29:14 +0100

    Improving CLI definition validator readability

--- shiftcontent/cli/definition.py
+++ shiftcontent/cli/definition.py
@@ -55,58 +55,59 @@ def validate_definition(path):
         print(green('Content type "{}": '.format(index)))
 
         for field, errors in data.items():
-            print(yellow('{} * {}:'.format(' ' * 4, field)))
 
             if type(errors) is list:
                 for type_field_err in errors:
-                    print('{} - {}'.format(' ' * 8, type_field_err))
+                    print('{} - {}'.format(' ' * 4, type_field_err))
 
-            if type(errors) is dict and 'direct' in errors:
-                for type_field_err in errors['direct']:
-                    print('{} - {}'.format(' ' * 8, type_field_err))
+            if type(errors) is dict:
+                print(yellow('{} * {}:'.format(' ' * 4, field)))
+                if 'direct' in errors:
+                    for type_field_err in errors['direct']:
+                        print('{} - {}'.format(' ' * 8, type_field_err))
 
-            if type(errors) is dict and 'collection' in errors:
-                for field_index, field_errors in errors['collection'].items():
-                    print_field(field_index, field_errors)
-                print()
+                if 'collection' in errors:
+                    fields = errors['collection']
+                    for field_index, field_errors in fields.items():
+                        print_field(field_index, field_errors)
 
     def print_field(index, data):
         """ Print field errors"""
-        print(magenta('{} - Field "{}":'.format(' ' * 8, index)))
+        print(green('{} * Field "{}":'.format(' ' * 8, index)))
 
         for field, errors in data.items():
-            print(cyan('{} * {}:'.format(' ' * 12, field)))
-
             if type(errors) is list:
                 for field_err in errors:
                     print('{} - {}'.format(' ' * 16, field_err))
 
-            if type(errors) is dict and 'direct' in errors:
-                for type_field_err in errors['direct']:
-                    print('{} - {}'.format(' ' * 16, type_field_err))
+            if type(errors) is dict:
+                print(yellow('{} * {}: '.format(' ' * 16, field)))
 
-            if type(errors) is dict and 'collection' in errors:
-                for field_index, field_errors in errors['collection'].items():
-                    print_filer_or_validator(
-                        field_index,
-                        field_errors,
-                        'Filter' if field == 'filters' else 'Validator'
-                    )
+                if type(errors) is dict and 'direct' in errors:
+                    for type_field_err in errors['direct']:
+                        print('{} - {}'.format(' ' * 20, type_field_err))
+
+                if 'collection' in errors:
+                    processors = errors['collection']
+                    for field_index, field_errors in processors.items():
+                        print_filer_or_validator(
+                            field_index,
+                            field_errors,
+                            'Filter' if field == 'filters' else 'Validator'
+                        )
 
     def print_filer_or_validator(index, data, object_type=None):
         """ Print filter/validator errors"""
         if not object_type:
             object_type = 'Filter/Validator'
 
-        print(green('{} - {} "{}":'.format(' ' * 16, object_type, index)))
+        print(green('{} * {} "{}":'.format(' ' * 20, object_type, index)))
         for prop, errors in data.items():
-            print(yellow('{} * {}:'.format(' ' * 20, prop)))
             for error in errors:
                 print('{} - {}'.format(' ' * 24, error))
 
-    from tests._assets.definition_errors import errors
-    # pp(errors)
-
+    errors = ok.get_messages()
+    
     content = errors['content']
     if 'direct' in content:
         print(red('Content types:'))
