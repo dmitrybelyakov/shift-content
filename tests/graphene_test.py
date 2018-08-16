0f476bcee059699e556c19c9a4dddc4449713deb
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2018-08-16 10:38:53 +0100

    Simple graphene test

---
+++ tests/graphene_test.py
@@ -0,0 +1,28 @@
+from tests.base import BaseTestCase
+from nose.plugins.attrib import attr
+
+import graphene
+
+
+@attr('graphene')
+class DbTest(BaseTestCase):
+
+    def test_define_simple_schema(self):
+        """ Simple graphene schema """
+
+        class Query(graphene.ObjectType):
+            hello = graphene.String(
+                name=graphene.String(default_value='stranger')
+            )
+
+            def resolve_hello(self, info, name):
+                return 'Hello {}'.format(name)
+
+
+        schema = graphene.Schema(query=Query)
+
+        graphql = '{ hello }'
+        result = schema.execute(graphql)
+
+        print(result.data['hello'])
+        breakpoint()
