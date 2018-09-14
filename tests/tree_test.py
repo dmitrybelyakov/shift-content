1a2e831131dcf77f9e7c45b9e210fa076ff10f75
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2018-09-14 12:19:00 +0100

    Tree building implementation

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -7,12 +7,31 @@ import random
 
 
 def build_tree(item, children):
-    print('BUILDING TREE NOW')
-    pp(item)
-    pp(children)
 
+    ids = list(child['id'] for child in children)
+    ids.append(item['id'])
 
+    # wrap nodes
+    item = dict(node=item, children=[])
+    children = [dict(node=c, children=[]) for c in children if all(
+        i in ids for i in c['path'].split('.')  # filter orphans
+    )]
 
+    def add_to_tree(item, tree):
+        is_child = tree['node']['id'] == item['node']['path'].split('.')[-1]
+        if is_child:
+            tree['children'].append(item)
+            del children[children.index(item)]
+        else:
+            tree['children'] = [add_to_tree(item, c) for c in tree['children']]
+
+        return tree
+
+    while children:
+        for child in children:
+            add_to_tree(child, item)
+
+    return item
 
 @attr('tree')
 class TreeTest(BaseTestCase):
@@ -36,8 +55,13 @@ class TreeTest(BaseTestCase):
             dict(name='An orphan', id='12', path='11.20.26.99'),
         ]
 
-        random.shuffle(children)
-        build_tree(root, children)
+        # random.shuffle(children)
+        tree = build_tree(root, children)
+
+        print('-'*80)
+        print('GOT TREE:')
+        pp(tree)
+        print('-'*80)
 
 
         
